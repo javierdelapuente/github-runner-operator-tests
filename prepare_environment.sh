a19aa642fe52978fe9de12c955a3baf258eb931d
Javier de la Puente <javier.delapuente@canonical.com>
2024-12-20 12:05:27 +0100

    first commit

---
+++ prepare_environment.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+set -euo pipefail
+
+. .secrets
+
+GITHUB_OWNER=javierdelapuente
+MICROSTACK_RISK=beta
+RAM_MEMORY=20GiB
+CPUS=12
+ROOT_DISK_SIZE=100GiB
+
+REPOSITORY=${GITHUB_OWNER}/github-runner-operator
+
+export MICROSTACK_RISK
+export REPOSITORY
+export GITHUB_TOKEN
+
+lxc delete openstack --force || :
+
+lxc init ubuntu:24.04 openstack --vm -c limits.cpu=${CPUS} -c limits.memory=${RAM_MEMORY} -d root,size=${ROOT_DISK_SIZE} --config=user.user-data="$(cat ./openstack-user-data | envsubst '$MICROSTACK_RISK,$REPOSITORY,$GITHUB_TOKEN')"
+
+echo "Starting at $(date)"
+lxc start openstack
+
+time retry -d 5 -t 5 lxc exec openstack -- cloud-init status --wait
+
+echo "End at $(date)"
